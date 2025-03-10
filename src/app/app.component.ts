7fe1ab034e4e4f8fdb4c961ff25a9966ad0de567
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-03-11 01:27:16 +0330

    feat: --> Sidebar Component | add sidebar component to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [SidebarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ticket-tracking';
