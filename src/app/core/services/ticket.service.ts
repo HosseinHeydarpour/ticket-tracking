6eec163041b3bd1acc62b460abd5cf3f43c686c4
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-03-12 09:01:19 +0330

    feat: --> Tickets Service | add tickets getter method

--- src/app/core/services/ticket.service.ts
+++ src/app/core/services/ticket.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
+import { TICKETS } from '../../tickets';
+import { Ticket } from '../../shared/report-card/models/ticket.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TicketService {
+  ticketsArr: Ticket[] = TICKETS;
+
   get tickets() {
-    return;
+    return this.ticketsArr;
   }
 }
