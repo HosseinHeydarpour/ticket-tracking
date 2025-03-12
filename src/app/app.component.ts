028c570821df24a2186aa708a9d2090560e5528b
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-03-12 11:06:27 +0330

    feat: --> App Component | set tickets on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { SidebarComponent } from './core/sidebar/sidebar.component';
 import { ReportCardComponent } from './shared/report-card/report-card.component';
+import { TicketService } from './core/services/ticket.service';
+import { Ticket } from './shared/report-card/models/ticket.model';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidebarComponent, ReportCardComponent],
+  imports: [SidebarComponent, ReportCardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ticket-tracking';
+export class AppComponent implements OnInit {
+  ticketService = inject(TicketService);
+  tickets = signal<Ticket[]>([]);
+
+  ngOnInit(): void {
+    this.tickets.set(this.ticketService.tickets);
+  }
 }
