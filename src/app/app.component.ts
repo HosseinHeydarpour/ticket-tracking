920535530cd182c5e7ee432619c557a5d7d15b59
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-03-13 00:33:24 +0330

    docs: --> App Component | add JSDoc comments for better documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,22 +12,50 @@ import { ThemesiwtcherComponent } from './shared/themesiwtcher/themesiwtcher.com
   selector: 'app-root',
   standalone: true,
   imports: [
-    SidebarComponent,
-    ReportCardComponent,
-    CommonModule,
-    ThemesiwtcherComponent,
+    SidebarComponent, // A component providing the sidebar functionality.
+    ReportCardComponent, // A shared component for displaying report cards.
+    CommonModule, // Angular's common directives like ngIf, ngFor, etc.
+    ThemesiwtcherComponent, // A shared component for switching themes.
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
+  /**
+   * @member {TicketService} ticketService
+   * @description
+   * An instance of the TicketService injected into the component.
+   * Used to retrieve and manage ticket-related data.
+   */
   ticketService = inject(TicketService);
+
+  /**
+   * @member {Signal<Ticket[]>} tickets
+   * @description
+   * A reactive signal holding an array of Ticket objects.
+   * This signal is updated with data from the TicketService during initialization.
+   */
   tickets = signal<Ticket[]>([]);
-  themeService = inject(ThemeService);
+
+  themeService = inject(ThemeService); // Inejecting theme service into app service
+
+  /**
+   * @member {Document} document
+   * @description
+   * A reference to the global `document` object, injected using Angular's `DOCUMENT` token.
+   * Used to manipulate the DOM, such as adding a theme class to the body element.
+   */
   document = inject(DOCUMENT);
 
+  /**
+   * @method ngOnInit
+   * @description
+   * lifecycle hook called once when the component is initialized.
+   * Sets the initial ticket data and applies the selected theme to the document body.
+   */
   ngOnInit(): void {
-    this.tickets.set(this.ticketService.tickets);
-    this.document.body.classList.add(this.themeService.theme());
+    this.tickets.set(this.ticketService.tickets); // Set the tickets signal with data from the TicketService.
+
+    this.document.body.classList.add(this.themeService.theme()); // Add the current theme class to the document body.
   }
 }
